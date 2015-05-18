40bf6b86174fa7aeec6a2aba4e211d08abdac276
Moritz Fischer <moritz.fischer@ettus.com>
2015-05-18 15:34:38 -0700

    e300: gps: Fix compiler issue when building without gpsd.
    Signed-off-by: Moritz Fischer <moritz.fischer@ettus.com>

--- host/lib/usrp/e300/e300_impl.cpp
+++ host/lib/usrp/e300/e300_impl.cpp
@@ -599,6 +599,7 @@ e300_impl::e300_impl(const uhd::device_addr_t &device_addr)
     _tree->access<subdev_spec_t>(mb_path / "rx_subdev_spec").set(rx_spec);
     _tree->access<subdev_spec_t>(mb_path / "tx_subdev_spec").set(tx_spec);
 
+#ifdef E300_GPSD
     //GPS installed: use external ref, time, and init time spec
     if (_gps and _gps->gps_detected()) {
         UHD_MSG(status) << "Setting references to the internal GPSDO" << std::endl;
@@ -606,10 +607,12 @@ e300_impl::e300_impl(const uhd::device_addr_t &device_addr)
         UHD_MSG(status) << "Initializing time to the internal GPSDO" << std::endl;
         const time_t tp = time_t(_gps->get_sensor("gps_time").to_int()+1);
         _tree->access<time_spec_t>(mb_path / "time" / "pps").set(time_spec_t(tp));
-    } else {
-        //init to internal clock and time source
-        _tree->access<std::string>(mb_path / "time_source/value").set("internal");
     }
+#else
+    //init to internal clock and time source
+    _tree->access<std::string>(mb_path / "time_source/value").set("internal");
+#endif// E300_GPSD
+
 }
 
 boost::uint8_t e300_impl::_get_internal_gpio(gpio_core_200::sptr gpio)
